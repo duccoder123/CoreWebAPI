849c1e28c91c64173657ee4a12a0c0e198e9045a
Teo <8duchuynh@gmail.com>
2023-09-11 16:01:14 +0700

    add api and using EF

---
+++ VillaAPI/Data/VillaStore.cs
@@ -0,0 +1,13 @@
+﻿using VillaAPI.Models.Dto;
+
+namespace VillaAPI.Data
+{
+    public static class VillaStore
+    {
+        public static List<VillaDTO> villaList = new List<VillaDTO>()
+            {
+                new VillaDTO{Id=1, Name="Pool View", Occupancy=100, Sqft=4},
+                new VillaDTO{Id=2, Name="Beach View", Occupancy = 200, Sqft =3 }
+            };
+    }
+}
--- VillaAPI/Program.cs
+++ VillaAPI/Program.cs
@@ -1,8 +1,14 @@
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
-builder.Services.AddControllers();
+//Log.Logger = new LoggerConfiguration().MinimumLevel.Debug().WriteTo.File("log/villaLogs.txt", rollingInterval:RollingInterval.Day).CreateLogger();
+//builder.Host.UseSerilog();
+builder.Services.AddControllers(options =>
+{
+    //options.ReturnHttpNotAcceptable = true;
+}).AddNewtonsoftJson().AddXmlDataContractSerializerFormatters() ;
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
